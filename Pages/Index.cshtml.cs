623c59a214391da80f8cb386eb2e01be70b71fcb
chopeen <chopeen@gmail.com>
2018-07-14 22:14:45 +0200

    added a model property, bound to the input textbox;

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
@@ -9,5 +10,20 @@ namespace last_fm_not_mine_alert_web.Pages
 {
     public class IndexModel : PageModel
     {
+        [BindProperty, Required, StringLength(50)]
+        public string ArtistName { get; set; }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            // for debugging only
+            Console.WriteLine(this.ArtistName);
+
+            return RedirectToPage("/Index");
+        }
     }
 }
