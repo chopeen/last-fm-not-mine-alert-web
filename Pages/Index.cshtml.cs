4316ca975bc8bc9d830f59a2cf1b92820a3e9ae4
chopeen <chopeen@gmail.com>
2018-07-22 12:59:41 +0200

    [WIP] replacing full-blown authentication with simple query string auth;

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -4,7 +4,6 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Headers;
-using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -28,12 +27,18 @@ namespace last_fm_not_mine_alert_web.Pages
         [BindProperty, Required, StringLength(50)]
         public string ArtistName { get; set; }
 
+        // TODO: Can this be implemented with the Authorize attribute? Custom authorization?
+        //
+        // TODO: This will work for the initial page load and fail when the token is not passed in `RedirectToPage` below.
+        //       Find a more robust solution - set a cookie and check "token or cookie"?
         public void OnGet()
         {
-            System.Security.Principal.IIdentity currentIdentity = ClaimsPrincipal.Current.Identity;
-            this._logger.LogInformation($"Logged user AuthenticationType: {currentIdentity.AuthenticationType}");
-            this._logger.LogInformation($"Logged user IsAuthenticated: {currentIdentity.IsAuthenticated}");
-            this._logger.LogInformation($"Logged user Name: {currentIdentity.Name}");
+            string authToken = this.Request.Query["code"];
+
+            if (string.IsNullOrEmpty(authToken) || authToken != this._configuration["WebsiteAuthToken"])
+            {
+                throw new System.Security.Authentication.InvalidCredentialException("No or wrong authentication token specified.");
+            }
         }
 
         public async Task<IActionResult> OnPostAsync()
